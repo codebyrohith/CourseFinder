16b772ecff537f4dc190de4f4efff7fe8971acb6
codewithrohith <rohithsaicharaneanugula@gmail.com>
2023-12-05 15:53:46 -0600

    minor changes in js file

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const dropdown = document.getElementById('dropdown');
   
     // Fetch data from AWS API Gateway with a readable stream
-    fetch('https://x21e74ohc3.execute-api.us-east-1.amazonaws.com/dev/categories',{
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin':'*',
-          'Access-Control-Allow-Methods':'GET'
-        },
-      })
+    fetch('https://your-api-endpoint.com/path')
       .then(response => {
         if (!response.ok) {
           throw new Error(`API request failed with status ${response.status}`);
@@ -19,30 +12,38 @@ document.addEventListener('DOMContentLoaded', () => {
         const reader = response.body.getReader();
   
         // Read stream asynchronously
-        return reader.read();
+        return readStream(reader);
       })
-      .then(function readStream({ done, value }) {
+      .catch(error => {
+        console.error('API Error:', error);
+      });
+  
+    function readStream(reader) {
+      return reader.read().then(({ done, value }) => {
         if (done) {
           console.log('Stream is done.');
           return;
         }
   
-        // Parse the value (assuming it's JSON)
-        const options = JSON.parse(value);
+        try {
+          // Parse the value (assuming it's JSON)
+          const options = JSON.parse(value);
   
-        // Populate dropdown with options
-        options.forEach(option => {
-          const optionElement = document.createElement('option');
-          optionElement.value = option.value;
-          optionElement.textContent = option.label;
-          dropdown.appendChild(optionElement);
-        });
+          // Populate dropdown with options
+          options.forEach(option => {
+            const optionElement = document.createElement('option');
+            optionElement.value = option.value;
+            optionElement.textContent = option.label;
+            dropdown.appendChild(optionElement);
+          });
   
-        // Continue reading the next chunk of the stream
-        return reader.read().then(readStream);
-      })
-      .catch(error => {
-        console.error('API Error:', error);
+          // Continue reading the next chunk of the stream
+          return readStream(reader);
+        } catch (parseError) {
+          console.error('JSON Parse Error:', parseError);
+          // Handle the error or abort reading the stream
+        }
       });
+    }
   });
   
