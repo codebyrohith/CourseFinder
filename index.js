ca0a6213340264bcab7a298e6a288004965c0fa4
codewithrohith <rohithsaicharaneanugula@gmail.com>
2023-12-05 13:17:40 -0600

    minor changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ async function populateDynamicDropdown() {
   
     try {
       // Make an API call to get data (replace 'your-api-endpoint' with the actual API endpoint)
-      const response = await fetch('https://6zs1a549rk.execute-api.us-east-1.amazonaws.com/dev');
+      const response = await fetch('https://ng9yb2ypsi.execute-api.us-east-1.amazonaws.com/dev/categories');
       
       if (!response.ok) {
         throw new Error(`Error: ${response.statusText}`);
