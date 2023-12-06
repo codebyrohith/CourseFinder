7bb200d8fc06344d411f43ac055586eb22c6a3e3
codewithrohith <rohithsaicharaneanugula@gmail.com>
2023-12-06 11:50:23 -0600

    working changes for dropdown

--- index.js
+++ index.js
@@ -1,62 +1,112 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const dropdown = document.getElementById('dropdown');
-  
-    // Fetch data from AWS API Gateway with a readable stream
-    fetch('https://x21e74ohc3.execute-api.us-east-1.amazonaws.com/dev/categories',
-    {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin':'*',
-          'Access-Control-Allow-Methods':'GET'
-        },
-      }
-    )
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`API request failed with status ${response.status}`);
-        }
-  
-        // Get a readable stream reader
-        const reader = response.body.getReader();
-  
-        // Read stream asynchronously
-        return readStream(reader);
-      })
-      .catch(error => {
-        console.error('API Error:', error);
-      });
-  
-    function readStream(reader) {
-      return reader.read().then(({ done, value }) => {
-        if (done) {
-          console.log('Stream is done.');
-          return;
-        }
-  
-        try {
-          // Parse the value (assuming it's JSON)
-          const jsonString = new TextDecoder().decode(value);
+var categoryselect = document.querySelector('.category')
 
-// Parse the string as JSON
-          const jsonObject = JSON.parse(jsonString);
-          const options = JSON.parse(jsonObject.body);
+async function loadCategories(){
+          try {
+            var dropdownOptions = []
+                const response = await fetch('https://x21e74ohc3.execute-api.us-east-1.amazonaws.com/dev/categories');
+          
+                if (!response.ok) {
+                  throw new Error(`API request failed with status ${response.status}`);
+                }
+          
+                const { body } = await response.json();
+                dropdownOptions = JSON.parse(body);
+                // console.log("hey!", dropdownOptions);
+                const option_var = document.getElementById('dropdownmenu');
+                // const values = ["a", "b,", "c", "d"];
+                // document.getElementById('dropdownmenu').options[0].text = dropdownOptions[0];
+                dropdownOptions.forEach(opt=>{
+                  dropdownmenu.add(new Option(opt, opt)); 
+                })
+              } catch (error) {
+                console.error('API Error:', error);
+              }
+
+}
+//DON'T REMOVE ANY COMMENTED LINES TILL WE FINISH OUR PROJECT!!!
+handleDropDownMenu = (params) => {
+  // console.log("vivek", params);
+  //use fetch(URL, {method:"POST", body:{}})
+  //create another function which gets table value and change accordingly
+}
+
+
+window.onload = loadCategories
+
+
+
+
+// // Ensure dropdownOptions is declared only once
+// if (!window.dropdownOptions) {
+//     window.dropdownOptions = [];
   
-          // Populate dropdown with options
-          options.forEach(option => {
-            const optionElement = document.createElement('option');
-            optionElement.value = option.value;
-            optionElement.textContent = option.label;
-            dropdown.appendChild(optionElement);
-          });
+//     async function fetchOptionsFromApi() {
+//       try {
+//         const response = await fetch('https://x21e74ohc3.execute-api.us-east-1.amazonaws.com/dev/categories', {
+//           method: 'GET',
+//           headers: {
+//             'Content-Type': 'application/json',
+//             'Access-Control-Allow-Origin': '*',
+//             'Access-Control-Allow-Methods': 'GET',
+//           },
+//         });
   
-          // Continue reading the next chunk of the stream
-          return readStream(reader);
-        } catch (parseError) {
-          console.error('JSON Parse Error:', parseError);
-          // Handle the error or abort reading the stream
-        }
-      });
-    }
-  });
+//         if (!response.ok) {
+//           throw new Error(`API request failed with status ${response.status}`);
+//         }
+  
+//         const { body } = await response.json();
+//         window.dropdownOptions = JSON.parse(body);
+  
+//         // Dispatch an event when options are ready
+//         const optionsReadyEvent = new Event('optionsReady');
+//         document.dispatchEvent(optionsReadyEvent);
+//       } catch (error) {
+//         console.error('API Error:', error);
+//       }
+//     }
+  
+//     document.addEventListener('DOMContentLoaded', () => {
+//       // Fetch options when the DOM is loaded
+//       fetchOptionsFromApi();
+  
+//       // Event listener for options ready
+//       document.addEventListener('optionsReady', () => {
+//         // Access dropdownOptions
+//         const dropdownOptions = window.dropdownOptions;
+  
+//         // Get reference to the dropdown element
+//         const dropdown = document.getElementById('dropdown');
+  
+//         // Check if dropdown is defined before populating
+//         if (dropdown) {
+//           // Populate dropdown with options
+//           dropdownOptions.forEach(option => {
+//             const optionElement = document.createElement('option');
+//             optionElement.value = option.value;
+//             optionElement.textContent = option.label;
+//             dropdown.appendChild(optionElement);
+//           });
+//         } else {
+//           console.error('Dropdown element not found.');
+//         }
+//       });
+  
+//       // Event listener for dropdown click
+//       function handleDropdownClick() {
+//         console.log('Dropdown options:', window.dropdownOptions);
+//         // Do something with the options, e.g., update UI
+//       }
+  
+//       // Assuming you have a dropdown element with id 'dropdown'
+//       const dropdown = document.getElementById('dropdown');
+//       if (dropdown) {
+//         dropdown.addEventListener('click', handleDropdownClick);
+//       } else {
+//         console.error('Dropdown element not found.');
+//       }
+//     });
+//   } else {
+//     console.error('dropdownOptions is already declared.');
+//   }
   
